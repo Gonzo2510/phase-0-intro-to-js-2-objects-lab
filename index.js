aaf99945dea2a25537cb285cc493cad551614757
jacobgonzales <jagonz@live.com>
2023-04-20 14:22:23 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution in this file!
+
+const employee = {
+    name: "Jacob Gonzales",
+    streetAddress: "2228 Blake St, Ste 100, · Denver, CO 80205" 
+};
+  
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return{
+        ...employee,
+        [key]: value,
+    };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] =  value
+    
+    return employee;
+}
+
+function  deleteFromEmployeeByKey(employee, key) {
+    const newObj = {...employee};
+    delete newObj[key]; 
+    return newObj
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key]
+    return employee;
+}
+
+
+
